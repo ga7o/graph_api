d6f4776f924dd5f8d8fa6250cd908e9ff647f1c9
ruialves <rui@kinsu.co.uk>
2019-03-04 12:38:48 +0000

    Adding mutations

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var schema = buildSchema(`
         course(id: Int!): Course
         courses(topic: String): [Course]
     }
-    
+    type Mutation {
+        updateCourseTopic(id:Int!, topic: String!): Course
+    }
     type Course {
         id: Int,
         title: String,
@@ -53,7 +55,6 @@ var getCourse = function (args) {
     })[0]
 };
 
-
 var getCourses = function (args) {
     if(args.topic){
         var topic = args.topic;
@@ -64,10 +65,21 @@ var getCourses = function (args) {
     }
 };
 
+var updateCourseTopic = function ({id, topic}) {
+    coursesData.map(course => {
+        if(course.id === id) {
+            course.topic = topic;
+            return course
+        }
+    });
+
+    return coursesData.filter(course => course.id === id)[0];
+}
 
 var root = {
     course: getCourse,
-    courses: getCourses
+    courses: getCourses,
+    updateCourseTopic: updateCourseTopic
 };
 
 // Create an express server and GraphQL endpoint
